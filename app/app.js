9f1694ec70e7dfbe5c186688ebb2eafd02661e13
timknapp12 <timknapp12@gmail.com>
2017-11-15 15:45:46 -0700

    first commit

--- app/app.js
+++ app/app.js
@@ -3,7 +3,37 @@ angular.module('devmtnTravel', ['ui.router']).config( function ( $stateProvider,
     .state('home',{
       url:'/',
       templateUrl: "app/about/aboutTmpl.html"
-    });
+    })
+
+    .state('packages', {
+      url: '/packages/:country',
+      templateUrl: 'app/packages/packagesTmpl.html',
+      controller: 'packagesCtrl'
+    })
+
+    .state('locations', {
+      url: '/locations',
+      templateUrl: 'app/locations/locationsTmpl.html',
+      controller: 'locationCtrl'
+    })
+
+    .state('booked', {
+      url: ('/booked/:id'),
+      templateUrl: 'app/booked/bookedTmpl.html',
+      controller: 'bookedCtrl'
+    })
+
+    .state('adventurers', {
+      url: '/adventureres',
+      templateUrl: 'app/about/adventurers/adventurersTmpl.html',
+      parent: 'home'
+    })
+
+    .state('contact', {
+      url: '/contact',
+      templateUrl: 'app/contact/contactTmpl.html',
+      parent: 'home'
+    })
 
   $urlRouterProvider
     .otherwise('/');
